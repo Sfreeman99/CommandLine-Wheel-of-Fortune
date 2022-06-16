fc4f8506367c08be85cc0441edec82bd5408a7c9
Shedlia Freeman <30321556+sfreeman99@users.noreply.github.com>
2022-06-16 01:38:30 -0500

    Bankruptcy sets current player score to 0

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -4,11 +4,12 @@ import java.util.List;
 import java.util.Random;
 
 public class Game {
-    List<Player> players = new ArrayList<>(5);
-    Random rand = new Random();
+    final List<Player> players = new ArrayList<>(5);
+    final Random rand = new Random();
     public int PLAYER_COUNT;
     public boolean START_GAME = false;
     public int PLAYER_TURN = 0;
+    final int BANKRUPT = 0;
     public void addPlayer(Player... players) {
         for (Player player : players) {
             this.players.add(player);
@@ -32,5 +33,8 @@ public class Game {
     }
 
     public void isBankrupt(int wheelNumber) {
+        if (wheelNumber == 250 || wheelNumber == 350) {
+            this.players.get(PLAYER_TURN).setScore(BANKRUPT);
+        }
     }
 }
